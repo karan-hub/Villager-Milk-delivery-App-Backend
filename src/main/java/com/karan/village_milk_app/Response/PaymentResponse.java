cd5c56bcc58f0b49c75145a9a31031924ef32007
karan <karanc4143@gmail.com>
2026-01-05 16:45:33 +0530

    feat(payment): add payment response model

--- src/main/java/com/karan/village_milk_app/Response/PaymentResponse.java
+++ src/main/java/com/karan/village_milk_app/Response/PaymentResponse.java
@@ -1,4 +1,10 @@
 package com.karan.village_milk_app.Response;
 
-public class PaymentResponse {
-}
+import com.karan.village_milk_app.model.Type.PaymentStatus;
+
+import java.util.UUID;
+
+public record PaymentResponse(
+        UUID paymentId,
+        PaymentStatus status
+) {}
