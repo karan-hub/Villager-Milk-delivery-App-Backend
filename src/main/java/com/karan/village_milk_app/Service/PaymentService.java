0eaa3dcaa10df4676e9d7596797c30ab9f4f2ecb
karan <karanc4143@gmail.com>
2026-01-05 16:45:34 +0530

    feat(payment): define payment service contract

--- src/main/java/com/karan/village_milk_app/Service/PaymentService.java
+++ src/main/java/com/karan/village_milk_app/Service/PaymentService.java
@@ -1,4 +1,15 @@
 package com.karan.village_milk_app.Service;
 
+import com.karan.village_milk_app.Request.InitiatePaymentRequest;
+import com.karan.village_milk_app.Response.PaymentResponse;
+import com.karan.village_milk_app.model.User;
+
+import java.util.UUID;
+
 public interface PaymentService {
+    public void confirmPayment(UUID paymentId);
+    public PaymentResponse initiatePayment(
+            User user,
+            InitiatePaymentRequest request
+    );
 }
