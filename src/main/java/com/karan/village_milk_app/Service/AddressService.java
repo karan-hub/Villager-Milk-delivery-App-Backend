068d79641b950e7071406fb96c29a284bac6f03b
karan <karanc4143@gmail.com>
2026-01-05 16:45:32 +0530

    feat(address): define address service contract

--- src/main/java/com/karan/village_milk_app/Service/AddressService.java
+++ src/main/java/com/karan/village_milk_app/Service/AddressService.java
@@ -1,4 +1,23 @@
 package com.karan.village_milk_app.Service;
 
+import com.karan.village_milk_app.Request.AddressRequest;
+import com.karan.village_milk_app.Response.AddressResponse;
+import com.karan.village_milk_app.model.Address;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface AddressService {
+
+    AddressResponse create(AddressRequest request);
+
+    List<AddressResponse> getMyAddresses();
+
+    AddressResponse getDefault();
+
+    AddressResponse update(UUID addressId, AddressRequest request);
+
+    void delete(UUID addressId);
+
+    AddressResponse markDefault(UUID addressId);
 }
