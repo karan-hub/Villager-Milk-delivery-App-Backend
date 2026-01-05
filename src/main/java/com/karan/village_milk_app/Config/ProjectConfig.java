501b026b25fde0256be3cf2c9b61db7dba7d2752
karan <karanc4143@gmail.com>
2026-01-05 16:45:36 +0530

    config(app): finalize application configuration

--- src/main/java/com/karan/village_milk_app/Config/ProjectConfig.java
+++ src/main/java/com/karan/village_milk_app/Config/ProjectConfig.java
@@ -11,6 +11,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 import java.time.Clock;
 
@@ -132,6 +133,8 @@ public class ProjectConfig {
     public Clock systemClock() {
         return Clock.systemUTC();
     }
+
+
 
 
 }
