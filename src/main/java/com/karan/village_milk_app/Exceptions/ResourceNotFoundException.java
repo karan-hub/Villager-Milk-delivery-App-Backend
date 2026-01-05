9b1d4e01921721400a20901bd18fdd5ff5933447
karan <karanc4143@gmail.com>
2026-01-05 16:45:35 +0530

    feat(exception): add resource not found exception

--- src/main/java/com/karan/village_milk_app/Exceptions/ResourceNotFoundException.java
+++ src/main/java/com/karan/village_milk_app/Exceptions/ResourceNotFoundException.java
@@ -1,11 +1,17 @@
 package com.karan.village_milk_app.Exceptions;
 
 public class ResourceNotFoundException   extends  RuntimeException  {
+    private   String resource;
+    private   String field;
+    private   String value;
+
     public ResourceNotFoundException(String msg) {
         super(msg);
     }
     public ResourceNotFoundException( ) {
         super("Resource Not Found  ");
     }
+
+
 }
 
