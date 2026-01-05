0f46feb8125e1fa371544f037dd19f05f0cfd090
karan <karanc4143@gmail.com>
2026-01-05 16:45:32 +0530

    feat(address): introduce address response DTO

--- src/main/java/com/karan/village_milk_app/Response/AddressResponse.java
+++ src/main/java/com/karan/village_milk_app/Response/AddressResponse.java
@@ -1,4 +1,27 @@
 package com.karan.village_milk_app.Response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
 public class AddressResponse {
+    private UUID id;
+
+    private String flatNumber;
+    private String buildingName;
+    private String area;
+    private String landmark;
+
+    private String city;
+    private String state;
+    private String pincode;
+    private String country;
+
+    private Double latitude;
+    private Double longitude;
+
+    private boolean isDefault;
 }
