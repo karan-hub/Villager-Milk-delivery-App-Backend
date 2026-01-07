dc3ad6bc79a1661294d9f0602752250553d3436a
karan <karanc4143@gmail.com>
2026-01-07 09:55:44 +0530

    feat(request): add CreateCustomSubscriptionRequest

---
+++ src/main/java/com/karan/village_milk_app/Request/CreateCustomSubscriptionRequest.java
@@ -0,0 +1,34 @@
+package com.karan.village_milk_app.Request;
+
+import com.karan.village_milk_app.Dto.DeliveryScheduleDto;
+import com.karan.village_milk_app.model.Type.DeliverySlot;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class CreateCustomSubscriptionRequest {
+
+    @NotNull
+    private UUID productId;
+
+
+
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @NotNull
+    private DeliverySlot deliverySlot;
+
+    @NotEmpty
+    private List<DeliveryScheduleDto> deliverySchedule;
+
+}
