19600f0d14c16a38960e7b01ef51fe8242a8f969
karan <karanc4143@gmail.com>
2026-01-07 09:55:45 +0530

    refactor(repository): enhance SubscriptionPlanRepository

--- src/main/java/com/karan/village_milk_app/Repositories/SubscriptionPlanRepository.java
+++ src/main/java/com/karan/village_milk_app/Repositories/SubscriptionPlanRepository.java
@@ -1,6 +1,6 @@
 package com.karan.village_milk_app.Repositories;
 
-import com.karan.village_milk_app.Response.SubscriptionDto;
+import com.karan.village_milk_app.Response.SubscriptionResponse;
 import com.karan.village_milk_app.model.SubscriptionPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
